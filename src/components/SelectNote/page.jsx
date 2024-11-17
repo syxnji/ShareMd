201be014fb69fafa5358ca22758b5f84f4fd6055
syxnji <syunji0710@gmail.com>
2024-11-17 14:32:01 +0900

    css 修正

--- src/components/SelectNote/page.jsx
+++ src/components/SelectNote/page.jsx
@@ -1,29 +1,34 @@
 import styles from "./selectNote.module.css"
+import Link from "next/link"
+
 export function Notes() {
     return(
-        <div className={styles.note}>
-            <div className={styles.top}>
-                <div className={styles.title}>
-                    <p>
-                        Note Title
-                    </p>
+        <Link href={"/"} className={styles.link}>
+            <div className={styles.note}>
+                
+                <div className={styles.top}>
+                    <div className={styles.title}>
+                        <p>
+                            Note Title
+                        </p>
+                    </div>
+                    <div className={styles.preview}>
+                        <p>
+                            This is preview ...
+                        </p>
+                    </div>
                 </div>
-                <div className={styles.preview}>
-                    <p>
-                        This is preview ...
-                    </p>
+                <div className={styles.bottom}>
+                    <div className={styles.history}>
+                        <p>
+                            Last Edited:
+                        </p>
+                        <p>
+                            1 Days ago
+                        </p>
+                    </div>
                 </div>
             </div>
-            <div className={styles.bottom}>
-                <div className={styles.history}>
-                    <p>
-                        Last Edited:
-                    </p>
-                    <p>
-                        1 Days ago
-                    </p>
-                </div>
-            </div>
-        </div>
+        </Link>
     )
 }
--- src/pages/library.jsx
+++
@@ -1,7 +0,0 @@
-export function Library() {
-    return (
-        <main>
-            
-        </main>
-    )
-}
