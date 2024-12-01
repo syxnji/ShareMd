dca389f5f0f5250f90d24346bbbd087cb098a1df
syxnji <syunji0710@gmail.com>
2024-12-01 21:15:34 +0900

    修正 フォルダ構成&src配下のpages対応

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    // pages ディレクトリを src 内に指定
+    experimental: {
+      srcDir: true,
+    },
+  };
+  
+  export default nextConfig;
