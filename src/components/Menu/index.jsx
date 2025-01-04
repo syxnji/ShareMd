5c71f04bec67ca7d10c4d0ec4459784227895fe8
syxnji <syunji0710@gmail.com>
2025-01-05 02:47:33 +0900

    修正 - デザイン & 新規ノート作成時に新しいグループをフェッチ

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,52 +1,147 @@
 'use client'
 import { useEffect, useState } from "react";
 // component
-import { ImgBtn } from "@/components/UI/ImgBtn";
-import { GroupHeadline } from "@/components/GroupHeadline";
-import { CreateGroup } from "@/components/CreateGroup";
+import { ToastContainer, toast } from 'react-toastify';
 // icon
 import { BsArrowBarLeft, BsBuildings, BsX } from "react-icons/bs";
-import { BsArrowBarRight } from "react-icons/bs";
 // style
 import styles from "./menu.module.css";
+import 'react-toastify/dist/ReactToastify.css';
 
-export function Menu({ setSelectedGroupId, userId }) {
+export function Menu({ setSelectedGroupId, userInfo, allGroups, fetchGroup }) {
 
-    // グループ表示
-    const [allGroups, setAllGroups] = useState([]);
-    const fetchGroup = async () => {
-        const response = await fetch(`/api/db?table=joinedGroups&userId=${userId}`);
-        const allGroups = await response.json();
-        setAllGroups(allGroups);
-    };
-    useEffect(() => {
-      fetchGroup();
-    }, [userId]);
-
-    // グループクリック
+    // MARK:グループ選択
     const groupClick = (id) => {
         setSelectedGroupId(id);
     }
 
-    // 表示/非表示
+    // MARK:メニュー表示/非表示
     const [isMenu, setIsMenu] = useState(true);
     const toggleMenu = () => {
         setIsMenu(!isMenu);
     };
 
-    // グループ追加モーダル
+    // MARK:グループ追加モーダル
     const [modalCreate, setModalCreate] = useState(false);
     const toggleModalCreate = () => {
         setModalCreate(!modalCreate);
     };
 
+    // MARK:グループ名
+    const [createName, setCreateName] = useState("");
+    const handleGroupName = (e) => {
+        setCreateName(e.target.value);
+    }
+
+    // MARK:メンバー検索
+    const [createMember, setCreateMember] = useState("");
+    const handleCreateMember = (e) => {
+        setCreateMember(e.target.value);
+    }
+
+    // MARK:メンバー候補
+    const [createMemberSuggest, setCreateMemberSuggest] = useState([]);
+    useEffect(() => {
+        const fetchMemberSuggest = async () => {
+            if (createMember.length > 0) {  
+                const response = await fetch(`/api/db?table=suggestUsers&name=${createMember}`);
+                const suggestUsers = await response.json();
+                setCreateMemberSuggest(suggestUsers.results);
+            } else {
+                setCreateMemberSuggest([]);
+            }
+        };
+        fetchMemberSuggest();
+    }, [createMember]);
+
+    // MARK:メンバー追加
+    const [memberList, setMemberList] = useState([]);
+    if (userInfo?.id && memberList.length === 0) {
+        setMemberList([{id: userInfo.id, username: userInfo.username}]);
+    }
+    const handleAddMember = (e, user) => {
+        e.preventDefault();
+        const newMember = {
+            id: user.id,
+            username: user.username
+        };
+        if (memberList.some(member => member.id === newMember.id)) {
+            toast.error('既に追加されています');
+            setCreateMember("");
+            return;
+        }
+        setMemberList([...memberList, newMember]);
+        setCreateMember("");
+    }
+
+    // MARK:メンバー削除
+    const handleDeleteMember = (e, memberToDelete) => {
+        e.preventDefault();
+        if (memberToDelete.id === userInfo.id) {
+            toast.error('自分は削除できません');
+        } else {
+            setMemberList(memberList.filter((m) => m.id !== memberToDelete.id));
+        }
+    }
+
+    // MARK:グループ作成
+    const handleCreateGroup = async (e) => {
+        e.preventDefault();
+        const memberIds = memberList.map((member) => member.id);
+        const response = await fetch(`/api/db?table=insertGroup&name=${createName}&memberIds=${memberIds}`);
+        const result = await response.json();
+        toggleModalCreate();
+        setCreateName("");
+        setMemberList([]);
+        fetchGroup();
+        toast.success('グループを作成しました');
+    }
+
+    // MARK:コンポーネント ━━━
     return(
         <>
-        <CreateGroup
-            fetchGroup={fetchGroup}
-            isOpen={modalCreate}
-            onClose={toggleModalCreate}
-        />
+        {/* MARK:トースト */}
+        <ToastContainer />
+
+        {/* MARK:グループ作成モーダル */}
+        {modalCreate ? (
+            <form className={styles.modalNewGroupWindow} onSubmit={handleCreateGroup}>
+                <button className={styles.modalNewGroupClose} onClick={toggleModalCreate}><BsX/></button>
+                <div className={styles.modalNewGroupContents}>
+                    <div className={styles.newGroupTitle}>
+                        <label className={styles.newGroupLabel}>グループ名</label>
+                        <input type="text" placeholder="例：チームα" className={styles.newGroupInput} onChange={handleGroupName} value={createName} required/>
+                    </div>
+                    <div className={styles.newGroupMembers}>
+                        <label className={styles.newGroupLabel}>メンバー</label>
+                        <input type="text" placeholder="ユーザー名で検索" className={styles.newGroupInput} onChange={handleCreateMember} value={createMember}/>
+                        {createMemberSuggest.length > 0 && (
+                            <div className={styles.newGroupSuggest}>
+                                <div className={styles.suggestMemberBox}>
+                                    {createMemberSuggest.map((user) => (
+                                        <button className={styles.suggestMember} key={user.id} onClick={(e) => handleAddMember(e, user)}>{user.username}</button>
+                                    ))}
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                    <div className={styles.newGroupMemberListBox}>
+                        <p className={styles.newGroupLabel}>メンバーリスト</p>
+                        <div className={styles.newGroupMemberList}>
+                        {memberList.map((member) => (
+                            <div className={styles.member} key={member.id}>
+                                <p>{member.username}</p>
+                                <button className={styles.memberDelete} onClick={(e) => handleDeleteMember(e, member)}><BsX/></button>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <button className={styles.newGroupBtn} type="submit">構築</button>
+                </div>
+            </form>
+        ) : null}
+
+        {/* MARK:メニュー */}
         <div className={isMenu ? styles.open : styles.close }>
             <button className={styles.resizeBtn} onClick={toggleMenu}>
                 <BsArrowBarLeft/>
